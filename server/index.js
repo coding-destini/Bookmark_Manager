ea426a8bcd037ac796872eecaf254f3e947ae479
coding-destini <akashshahngu@gmail.com>
2024-04-01 00:31:04 +0530

    cors config

--- server/index.js
+++ server/index.js
@@ -8,13 +8,7 @@ const cors = require('cors')
 app.use(cors());
 app.use(express.json());
 
-// Add CORS headers middleware
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); 
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
-    next();
-});
+
 
 app.use(require('./routes/index'))
 
