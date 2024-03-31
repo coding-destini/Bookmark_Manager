39f4c3fe35214ef3dabeae049e6381d85f9851d2
coding-destini <akashshahngu@gmail.com>
2024-04-01 00:27:34 +0530

    cors config

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ app.use(express.json());
 
 // Add CORS headers middleware
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
     next();
 });
 
